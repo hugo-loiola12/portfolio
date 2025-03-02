c1afef27bd2a4f08588e3528caa5f5cdc89df48f
Hugo Loiola <hugoloiola.1@gmail.com>
2025-03-02 01:39:11 -0300

    feat: pikachu

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         Meu Portifólio
       </h1>
       <p className="mb-4">{`Olá, eu sou o hugo.`}</p>
-      <Image src={"/images/hugo.webp"} alt="Hugo" width={200} height={200} />
+      <Image src={"/hugo.png"} alt="Hugo" width={200} height={200} />
       <div className="my-8">
         <BlogPosts />
       </div>
